2026b0bc1c3ed9d8d82954196fe41fbc893904a7
Benjamin <benj@zagorski.com>
2017-07-03 23:00:43 -0700

    JSON viewer is sorta working, but not with the size we're looking for.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,44 @@
-import JSONFormatter from 'json-formatter-js'
+// import JSONFormatter from 'json-formatter-js'
+//
+// const myJSON = {ans: 42};
+//
+// const formatter = new JSONFormatter(myJSON);
+//
+// document.body.appendChild(formatter.render());
 
-const myJSON = {ans: 42};
+// var data = {
+//   "foobar": {"groan": "foobaz"}
+// };
 
-const formatter = new JSONFormatter(myJSON);
+$("#uploadedFile").on("change", function() {
+  var data = $("#uploadedFile")[0].files;
 
-document.body.appendChild(formatter.render());
+  var fr = new FileReader();
+
+  fr.onload = function(e) {
+    console.log(e);
+    var result = JSON.parse(e.target.result);
+    // var formatted = JSON.stringify(result, null, 2);
+    // console.log(formatted);
+    $('#json-renderer').jsonViewer(result, {collapsed: true});
+  };
+  fr.readAsText(data.item(0));
+
+});
+// $("#uploadedFile").on("load", function() {
+//   var data = $("#uploadedFile")[0].files;
+//
+//   var fr = new FileReader();
+//
+//   fr.onload = function(e) {
+//     console.log(e);
+//     var result = JSON.parse(e.target.result);
+//     // var formatted = JSON.stringify(result, null, 2);
+//     // console.log(formatted);
+//     $('#json-renderer').jsonViewer(result, {collapsed: true});
+//   };
+//   fr.readAsText(data.item(0));
+//
+// });
+
+// console.log(data);
